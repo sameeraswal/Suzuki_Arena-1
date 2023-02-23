07dd00e0519a8dc1bcd9b0d317ea97a6859ef478
Himanshu664 <gs2012023@sgsitsindore.in>
2023-02-23 22:19:39 +0530

    29 commit

--- src/Components/leaderboard/Leaderboard.jsx
+++ src/Components/leaderboard/Leaderboard.jsx
@@ -11,64 +11,66 @@ const Leaderboard = () => {
           <div className="description">
             <h3>Leaderboard</h3>
           </div>
-          <table>
-            <thead>
-              <tr>
-                <td></td>
-                <td>Player</td>
-                <td>Score</td>
-                <td>Average</td>
-              </tr>
-            </thead>
+          <div className="table-div">
+            <table>
+              <thead>
+                <tr>
+                  <td></td>
+                  <td>Player</td>
+                  <td>Score</td>
+                  <td>Average</td>
+                </tr>
+              </thead>
 
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>
-                  <p id="winner">A</p>
-                </td>
-                <td>239</td>
-                <td>12.54</td>
-              </tr>
-              <br />
-              <tr>
-                <td>2</td>
-                <td>
-                  <p id="runner-up">B</p>
-                </td>
-                <td>209</td>
-                <td>10.2</td>
-              </tr>
-              <br />
-              <tr>
-                <td>3</td>
-                <td>
-                  <p id="second-runner-up">C</p>
-                </td>
-                <td>154</td>
-                <td>8.4</td>
-              </tr>
-              <br />
-              <tr>
-                <td>4</td>
-                <td>
-                  <p>D</p>
-                </td>
-                <td>100</td>
-                <td>3.1</td>
-              </tr>
-              <br />
-              <tr>
-                <td>5</td>
-                <td>
-                  <p>E</p>
-                </td>
-                <td>82</td>
-                <td>2.0</td>
-              </tr>
-              <br />
-            </tbody>
-          </table>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <p id="winner">A</p>
+                  </td>
+                  <td>239</td>
+                  <td>12.54</td>
+                </tr>
+                <br />
+                <tr>
+                  <td>2</td>
+                  <td>
+                    <p id="runner-up">B</p>
+                  </td>
+                  <td>209</td>
+                  <td>10.2</td>
+                </tr>
+                <br />
+                <tr>
+                  <td>3</td>
+                  <td>
+                    <p id="second-runner-up">C</p>
+                  </td>
+                  <td>154</td>
+                  <td>8.4</td>
+                </tr>
+                <br />
+                <tr>
+                  <td>4</td>
+                  <td>
+                    <p>D</p>
+                  </td>
+                  <td>100</td>
+                  <td>3.1</td>
+                </tr>
+                <br />
+                <tr>
+                  <td>5</td>
+                  <td>
+                    <p>E</p>
+                  </td>
+                  <td>82</td>
+                  <td>2.0</td>
+                </tr>
+                <br />
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
