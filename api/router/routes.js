8669082b13ae5fcb5549647056986d2158965a43
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-07 16:49:56 +0530

    corrected API

--- api/router/routes.js
+++ api/router/routes.js
@@ -1,42 +1,59 @@
 const express = require("express");
 const router = express.Router();
-const employeeController = require("../controller/employee")
-const roundController = require("../controller/round")
+// const employeeController = require("../controller/employee")
+// const roundController = require("../controller/round")
 //const jwt = require("jsonwebtoken")
 const employeeController = require("../controller/employeeController");
 const employeeReportController = require("../controller/employeeReportController");
 const roundController = require("../controller/roundController");
 
-
-
-//router.post("/api/v1/addround", roundController.addRound) 
+//router.post("/api/v1/addround", roundController.addRound)
 router.get("/api/v1/mspin/:mspin", employeeController.getEmployeeDetails);
 router.post("/api/v1/employee/register", employeeController.registerEmployee);
 router.post("/api/v1/login", employeeController.checkLogin);
 router.get("/api/v1/roundlists", roundController.getRoundLists);
 router.get("/api/v1/roundname/:roundName", roundController.getRoundDetails);
-router.post("/api/v1/round/submitanswer", employeeReportController.submitAnswerOfQuestion);
+router.post(
+  "/api/v1/round/submitanswer",
+  employeeReportController.submitAnswerOfQuestion
+);
 //submit round 2 Card Answer
-router.post("/api/v1/round/submitcardanswer", employeeReportController.submitAnswerOfCardQuestion);
+router.post(
+  "/api/v1/round/submitcardanswer",
+  employeeReportController.submitAnswerOfCardQuestion
+);
 
-router.post("/api/v1/quiz/employee/currentscore", employeeReportController.calculateCurrentScoreOfEmp);
+router.post(
+  "/api/v1/quiz/employee/currentscore",
+  employeeReportController.calculateCurrentScoreOfEmp
+);
 
-router.post("/api/v1/round/roundscore", employeeReportController.calculateScoreOfOneRound); //this should be get or post?
+router.post(
+  "/api/v1/round/roundscore",
+  employeeReportController.calculateScoreOfOneRound
+); //this should be get or post?
 
+router.get(
+  "/api/v1/round/roundscore/mspin/:mspin/roundname/:roundname",
+  employeeReportController.getRoundScore
+); //this should be get or post?
+router.get(
+  "/api/v1/quiz/finalscore/mspin/:mspin",
+  employeeReportController.getFinalScoreOfAllRounds
+);
 
-
-router.get("/api/v1/round/roundscore/mspin/:mspin/roundname/:roundname", employeeReportController.getRoundScore); //this should be get or post?
-router.get("/api/v1/quiz/finalscore/mspin/:mspin", employeeReportController.getFinalScoreOfAllRounds);
-
-router.get("/api/v1/quiz/finalscore/leaderboard", employeeReportController.getScoreOfEveryone);
+router.get(
+  "/api/v1/quiz/finalscore/leaderboard",
+  employeeReportController.getScoreOfEveryone
+);
 
 //router.get("/api/v1/quiz/mspin/:mspin/finalscore", employeeReportController.getFinalScore); //this should be get or post?
 //router.post("/api/v1/round/saveEachAnswer", employeeReportController.saveEachAnswer)
 //router.post("/api/v2/round/submitOneAnswer", employeeReportController.submitOneAnswer)
 
-router.get("/api/v1/mspin/:mspin", employeeController.getEmployeeDetails)
-router.post("/api/v1/login", employeeController.checkLogin)
-router.get("/api/v1/roundlists", roundController.getRoundLists)
-router.get("/api/v1/roundname/:roundName", roundController.getRoundDetails)
+router.get("/api/v1/mspin/:mspin", employeeController.getEmployeeDetails);
+router.post("/api/v1/login", employeeController.checkLogin);
+router.get("/api/v1/roundlists", roundController.getRoundLists);
+router.get("/api/v1/roundname/:roundName", roundController.getRoundDetails);
 
 module.exports = router;
