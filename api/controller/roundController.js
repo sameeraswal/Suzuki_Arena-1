bbfa8d4c4e0aad92826221190195ccad82d3c249
abhisheksingh <singhabhishek@paisabazaar.com>
2023-03-08 21:16:48 +0530

    changes done

--- api/controller/roundController.js
+++ api/controller/roundController.js
@@ -1,5 +1,7 @@
 require("../db/connection");
 const Round = require("../model/roundsSchema");
+//const employeeUnlockedRound = require("../model/employeeUnlockedRound"); Roundunlocked
+const Roundunlocked = require("../model/employeeUnlockedRound");
 
 // exports.addRound = async(req,res) =>{
 //     const quizName = new Round({
@@ -125,16 +127,55 @@ const Round = require("../model/roundsSchema");
 exports.getRoundLists = async (req, res) => {
     let status = false;
     try {
-        const rounds = await Round.find({}).select({ roundName: 1, isRoundLocked: 1, _id: 0 });
+        const mspin = req.body.mspin;
+        const name = req.body.name;
+        //const roundName= req.body.roundName;
+        //const rounds = await Round.find({}).select({ roundName: 1, isRoundLocked: 1, _id: 0 });
+        const rounds = await Round.find({}).select({ roundName: 1, _id: 0 }).sort({ roundOrder: 1 });
         //const rounds = await Round.find().select("roundName -_id");
-        //console.log(rounds)
+        console.log("rounds before updataion=========", rounds)
         if (rounds.length) {
-            rounds.push({ title: "ROUNDS" });
             status = true;
-            res.status(201).json({
-                status: status,
-                data: rounds
-            })
+
+            let disabledRound = await Roundunlocked.findOne({ mspin: mspin, name: name }).select({ disabled: 1 });
+            //console.log("disabledRounddddddddddddd==========",disabledRound)
+            if (disabledRound) {
+                let disabledRounds = disabledRound.disabled;
+                //console.log(disabledRounds)
+                console.log("disabledRoundssssss from collection==========", disabledRounds)
+
+
+                rounds.forEach((round, index, rounds) => {
+                    //console.log("inside foreach loop rounddddddd=======", round)
+                    let checkRoundNameExists = disabledRounds.includes(round.roundName)
+
+                    if (checkRoundNameExists) {
+                        rounds[index]["isRoundLocked"] = true;
+                        //round["isRoundLocked"] = false;
+                    } else {
+                        rounds[index]["isRoundLocked"] = false;
+                        //round["isRoundLocked"] = true;
+                    }
+                })
+                console.log("rounds aftrer updataion=========", rounds)
+                // disabledRounds.push(roundName);
+                
+                // disabledRound.shift();
+                // console.log(disabledRounds);
+
+                res.status(201).json({
+                    status: status,
+                    data: rounds
+                })
+
+            } else {
+                status = false
+                res.status(401).json({
+                    status: status,
+                    message: "data not found"
+                })
+
+            }
         } else {
             res.status(404).json({
                 status: status,
@@ -142,7 +183,7 @@ exports.getRoundLists = async (req, res) => {
             })
         }
     } catch (error) {
-        res.json({
+        res.status(404).json({
             status: status,
             error: error
         })
@@ -177,6 +218,78 @@ exports.getRoundDetails = async (req, res) => {
             status: status,
             error: error
         })
+    }
+}
+
+exports.getWheelTitles = async (req, res) => {
+    let status = false;
+    try {
+        const roundName = req.params.roundName;
+        const round = await Round.findOne({ roundName: roundName }, { "questions.wheelQuestiontitle": 1, _id: 0 });
+        console.log(round, roundName)
+        if (round) {
+            const wheelTitle = round.questions;
+            status = true;
+            return res.status(201).json({
+                message: status,
+                data: wheelTitle
+            });
+        } else {
+            return res.status(404).json({
+                message: status,
+                data: "data not found"
+            });
+        }
+
+    } catch (error) {
+        res.status(404).json({
+            status: status,
+            error: error
+        })
+
+    }
+}
+
+exports.getCardsTitles = async (req, res) => {
+    let status = false;
+    try {
+        const roundName = req.params.roundName;
+        const wheelQuestionId = req.params.wheelQuestionId;
+        //const round = await Round.findOne({ roundName: roundName, "questions.wheelQuestionId ": wheelQuestionId},{"questions.cardQuestions":1})
+        //const round = await Round.find({$and: [{roundName: 2}, {questions:{"questions.wheelQuestionId": 2}}]},{"questions.cardQuestions":1})
+        const round = await Round.find({ roundName: roundName, "questions.wheelQuestionId": wheelQuestionId }, { "questions.cardQuestions": 1 });
+        //const round = await Round.findOne({ roundName: roundName});
+        console.log(round)
+        let q = {};
+        round.forEach((obj) => {
+            q["xyz"] = obj.questions
+
+
+        })
+        console.log(q)
+        //console.log(round)
+        //, { "questions.wheelQuestiontitle": 1, _id: 0 }
+        if (round) {
+            //const wheelTitle = round.questions;
+            //let arrayOfCardDeatils = round.questions
+            status = true;
+            return res.status(201).json({
+                message: status,
+                data: round
+            });
+        } else {
+            return res.status(404).json({
+                message: status,
+                data: "data not found"
+            });
+        }
+
+    } catch (error) {
+        res.status(404).json({
+            status: status,
+            error: error
+        })
+
     }
 }
 
