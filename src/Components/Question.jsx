f6c2ff62336bf01f6513f104f79da3231527c481
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-10 00:47:49 +0530

    Added Question page

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -12,7 +12,60 @@ import { bgImg } from "./NewImages/Spin-Wheel_BG.png";
 
 const Question = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const imgsrc = require("./stop.png");
 
+  // timer
+  const Ref = useRef(null);
+
+  const [timer, setTimer] = useState("01:00");
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  const startTimer = (e) => {
+    let { total, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      setTimer(
+        (minutes > 9 ? minutes : "0" + minutes) +
+          ":" +
+          (seconds > 9 ? seconds : "0" + seconds)
+      );
+    }
+  };
+
+  const clearTimer = (e) => {
+    setTimer("01:00");
+
+    if (Ref.current) clearInterval(Ref.current);
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+    deadline.setSeconds(deadline.getSeconds() + 60);
+    return deadline;
+  };
+
+  useEffect(() => {
+    clearTimer(getDeadTime());
+  }, []);
+
+  const onClickReset = () => {
+    clearTimer(getDeadTime());
+  };
   const close = () => {
     setModalOpen(false);
   };
@@ -126,8 +179,55 @@ const Question = () => {
 
       <div className="question-container">
         <div className="ques-number">{`${activeQuestion + 1}/5`}</div>
-        <Timer setOpenModal={setOpenModal} />
-
+        {/* <Timer setOpenModal={setOpenModal}/> */}
+        <div className="timer-div">
+          <h2>{timer}</h2>
+          <img src={imgsrc} alt="" className="clock-img" />
+        </div>
+        <div>
+          {timer === "00:00" && openModel && (
+            <>
+              <div
+                className="modalBackground"
+                style={{ zIndex: "1", marginLeft: "-10%" }}
+              >
+                <div className="modalContainer">
+                  <div className="titleCloseBtn">
+                    <button
+                      style={{
+                        position: "absolute",
+                        color: "red",
+                        fontSize: "30px",
+                      }}
+                      onClick={() => {
+                        setOpenModal(false);
+                      }}
+                    ></button>
+                  </div>
+                  <div className="title">
+                    <h1 style={{ color: "blue" }}>
+                      Time's up! Click on continue to move to next question
+                    </h1>
+                  </div>
+                  <div className="footer">
+                    <button
+                      onClick={() => {
+                        setOpenModal(false);
+                        onClickNext();
+                        fetchAnswer();
+                        onClickReset();
+                      }}
+                      id="cancelBtn"
+                      className="third icon-conatiner"
+                    >
+                      Continue
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
         {v && (
           <div className="question-video">
             {/* {console.log(v.video)} */}
@@ -170,6 +270,7 @@ const Question = () => {
                 onClick={() => {
                   onClickNext();
                   fetchAnswer();
+                  onClickReset();
                 }}
                 className="third question-btn icon-conatiner"
               >
