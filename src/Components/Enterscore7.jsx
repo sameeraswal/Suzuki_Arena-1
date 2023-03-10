44a2e5fdcb90056880a712eb2c87aa33ce8187a6
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-10 21:44:25 +0530

    Added Assets

--- src/Components/Enterscore7.jsx
+++ src/Components/Enterscore7.jsx
@@ -19,6 +19,15 @@ const Enterscore7 = () => {
   const [score, setScore] = useState("");
 
   const [show, setShow] = useState(false);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const close = () => {
+    setModalOpen(false);
+  };
+  const open = () => {
+    setModalOpen(true);
+  };
   const handleChangescore = (event) => {
     setScore(event.target.value);
   };
@@ -73,17 +82,29 @@ const Enterscore7 = () => {
           Enter Score
         </button>
       </div>
-      <Link to="/">
-        <button
-          className="roll icon-conatiner finish-card finish-btn-enter-score"
-          onClick={() => {
-            fetchData();
-            getData();
-          }}
-        >
-          Finish Round
-        </button>
-      </Link>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="roll icon-conatiner finish-card finish-btn-enter-score"
+        onClick={() => {
+          modalOpen ? close() : open();
+          fetchData();
+          getData();
+        }}
+      >
+        Finish Round
+      </motion.button>
+
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        // Fires when all exiting nodes have completed animating out
+        onExitComplete={() => null}
+      >
+        {modalOpen && (
+          <Finishmodalenter modalOpen={modalOpen} handleClose={close} />
+        )}
+      </AnimatePresence>
     </div>
   );
 };
