9221c92896435a95b7b16fc0515c3b3f3ffce379
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-10 20:57:32 +0530

    Leaderboard done

--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -177,10 +177,10 @@ const Question = () => {
       <Navbar />
 
       <div className="round-box">Round 1-A</div>
-      
+
       <div className="question-container">
         <div className="ques-number">{`${activeQuestion + 1}/5`}</div>
-        
+
         <div className="timer-div">
           <h2>{timer}</h2>
           <img src={imgsrc} alt="" className="clock-img" />
@@ -270,17 +270,23 @@ const Question = () => {
           {/* modal */}
           <div>
             {activeQuestion !== 4 ? (
-              <button
-                onClick={() => {
-                  onClickNext();
-                  fetchAnswer();
-                  onClickReset();
-                  setOpenModal(true);
-                }}
-                className="third question-btn icon-conatiner"
-              >
-                Submit
-              </button>
+              cid > 0 ? (
+                <button
+                  onClick={() => {
+                    onClickNext();
+                    fetchAnswer();
+                    onClickReset();
+                    setOpenModal(true);
+                  }}
+                  className="third question-btn icon-conatiner"
+                >
+                  Submit
+                </button>
+              ) : (
+                <button className="third question-btn icon-conatiner please-chose">
+                  Please Chose an Option
+                </button>
+              )
             ) : (
               <>
                 <motion.button
@@ -293,9 +299,7 @@ const Question = () => {
                 </motion.button>
 
                 <AnimatePresence
-                 
                   initial={false}
-                 
                   exitBeforeEnter={true}
                   // Fires when all exiting nodes have completed animating out
                   onExitComplete={() => null}
--- src/Components/leaderboardfinal/leaderboardth.jsx
+++ src/Components/leaderboardfinal/leaderboardth.jsx
@@ -17,11 +17,11 @@ const Leaderboardth = () => {
           <img src={trophy} alt="" className="resp-trophy"/>
         </div>
         <img src={f} alt="" className="first"/>
-        <h1 className="position-text align-lead-1">Anvesh Mishra</h1>
+        <h1 className="position-text align-lead-1">Abhishek</h1>
         <img src={s} alt="" className="second"/>
-        <h1 className="position-text align-lead-2">Garvit Garg</h1>
+        <h1 className="position-text align-lead-2">Himanshu Malviya New</h1>
         <img src={t} alt="" className="thirdlast"/>
-        <h1 className="position-text align-lead-3">Abhinav Jain</h1>
+        <h1 className="position-text align-lead-3">Himanshu</h1>
       </div>
     </div>
   );
