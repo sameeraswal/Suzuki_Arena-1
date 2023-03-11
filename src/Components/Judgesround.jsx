1393e443bc941adc0602fb3286bbcb75e553619b
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-11 16:05:30 +0530

    Corrected collection

--- src/Components/Judgesround.jsx
+++ src/Components/Judgesround.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import modalImg from "../Components/ModalImages/Judges-Round.png";
+import modalImg from '../Components/ModalImages/Judges-Round.png'
 import "./modalcss.css";
 
 import ModalFrammere from "./Modalframmer/Modalframmere";
@@ -25,12 +25,12 @@ const Judgesround = () => {
       <Navbar />
       <div className="">
         <motion.button
-        // whileHover={{ scale: 1.1 }}
-        // whileTap={{ scale: 0.9 }}
-        // className="save-button modal-container"
-        // onClick={() => (modalOpen ? close() : open())}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="save-button modal-container"
+          onClick={() => (modalOpen ? close() : open())}
         >
-          <img
+         <img
             src={modalImg}
             alt=""
             height={550}
@@ -41,7 +41,7 @@ const Judgesround = () => {
               marginLeft: "-10px",
               borderRadius: "16px",
             }}
-          />
+            />
           <h2>Judges Round</h2>
         </motion.button>
       </div>
