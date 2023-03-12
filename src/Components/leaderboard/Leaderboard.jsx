5ad1a8fe3b3e6a2abc57c09dfaf7ff7cf797b391
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-12 15:42:45 +0530

    Completed leaderboard

--- src/Components/leaderboard/Leaderboard.jsx
+++ src/Components/leaderboard/Leaderboard.jsx
@@ -77,32 +77,6 @@ const Leaderboard = () => {
 
           <div className="table-div">
             <div className="table-heading">
-              {/* <img src={bg} alt="" className="bg bg1" />
-              <h1 className="text-on-img">Registration No</h1>
-              <img src={bg} alt="" className="bg bg2" />
-              <h1 className="text-on-img2">Name</h1> */}
-
-              {/* <img src={bg} alt="" className="bg bg4" />
-              <h1 className="text-on-img3">Round 1-A</h1>
-              <img src={bg} alt="" className="bg bg5" />
-              <h1 className="text-on-img4">Round 1-B</h1>
-              <img src={bg} alt="" className="bg bg6" />
-              <h1 className="text-on-img5">Round 2</h1>
-              <img src={bg} alt="" className="bg bg7" />
-              <h1 className="text-on-img6">Round 3</h1>
-              <img src={bg} alt="" className="bg bg8" />
-              <h1 className="text-on-img7">Round 4</h1>
-              <img src={bg} alt="" className="bg bg9" />
-              <h1 className="text-on-img8">Round 5</h1>
-              <img src={bg} alt="" className="bg bg10" />
-              <h1 className="text-on-img9">Round 6</h1>
-              <img src={bg} alt="" className="bg bg11" />
-              <h1 className="text-on-img10">Round 7</h1> */}
-
-              {/* <img src={bg} alt="" className="bg bg5" />
-              <h1 className="text-on-img11">Total Score</h1>
-              <img src={bg} alt="" className="bg bg3" />
-              <h1 className="text-on-img12">Rank</h1> */}
             </div>
             <table>
               <tbody>
--- src/Components/leaderboardfinal/leaderboardth.jsx
+++ src/Components/leaderboardfinal/leaderboardth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import "./leaderboardth.css";
 import text from "./01.png";
@@ -6,22 +6,53 @@ import trophy from "./09.png";
 import f from "./02.png";
 import s from "./03.png";
 import t from "./04.png";
+import axios from "axios";
+import { APIURL } from "../../App";
 
 const Leaderboardth = () => {
+  const [round, setRound] = useState([]);
+  const [name1, setName1] = useState([]);
+  const [name2, setName2] = useState([]);
+
+  const [name3, setName3] = useState([]);
+
+  useEffect(() => {
+    // alert("Page is running");
+    // fetchData();
+    axios
+      .get(`${APIURL}/api/v1/quiz/scoreboard`)
+      .then((res) => {
+        setRound(res.data.data.scoreBoard);
+        setName();
+        console.log(res);
+      })
+      .catch((error) => console.log(error, "error is here"));
+  }, []);
+  const setName = () => {
+    for (let i = 0; i < round.length; i++) {
+      console.log(round[i].rank);
+      round[i].rank === 1 ? setName1(round.name1) : setName1(round.name1);
+    }
+  };
+
   return (
     <div>
       <Navbar />
       <div className=" bg-img">
         <div className="description">
-          <img src={trophy} alt="" className="resp-trophy resp-trophy-1"/> <img src={text} alt="" className="resp-text" />
-          <img src={trophy} alt="" className="resp-trophy"/>
+          <img src={trophy} alt="" className="resp-trophy resp-trophy-1" />{" "}
+          <img src={text} alt="" className="resp-text" />
+          <img src={trophy} alt="" className="resp-trophy" />
         </div>
-        <img src={f} alt="" className="first"/>
-        <h1 className="position-text align-lead-1">Abhishek</h1>
-        <img src={s} alt="" className="second"/>
-        <h1 className="position-text align-lead-2">Himanshu Malviya New</h1>
-        <img src={t} alt="" className="thirdlast"/>
-        <h1 className="position-text align-lead-3">Himanshu</h1>
+        {/* {round.map((item) => ( */}
+        <>
+          <img src={f} alt="" className="first" />
+          <h1 className="position-text align-lead-1">{name1}</h1>
+          <img src={s} alt="" className="second" />
+          <h1 className="position-text align-lead-2">{name2}</h1>
+          <img src={t} alt="" className="thirdlast" />
+          <h1 className="position-text align-lead-3">{name3}</h1>
+        </>
       </div>
     </div>
   );
