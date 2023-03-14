d3bb8a4b284440b117b01448025df11178632a5c
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-15 00:28:29 +0530

    enterscore i

--- src/Components/Random Number/Randomnumber.js
+++ src/Components/Random Number/Randomnumber.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./randomnum.css";
 import Navbar from "../Navbar";
 import { Link } from "react-router-dom";
-import randomnumimg from "./saucer final (1) (online-video-cutter.com).mp4";
+import randomnumimg from "./random.mp4";
 import ReactPlayer from "react-player";
 import randomvideo from "./random.mp4";
 
@@ -35,77 +35,41 @@ export default function Random() {
     <>
       <Navbar />
 
-      {/* <div className="hero"> */}
-      {/* <div className="round-box ">Number Generator</div> */}
-      {/* <div className="round-box">Random Number Generator</div> */}
+<div className="hero">
+  {/* <div className="round-box ">Number Generator</div> */}
+  {/* <div className="round-box">Random Number Generator</div> */}
 
+  <iframe
+    src={randomnumimg}
+    alt=""
+    height={550}
+    width={1000}
+    style={{
+      marginLeft: "-30px",
+      marginBottom: "-350px",
+      paddingLeft: "-300px",
+    }}
+    className="randomno-video"
+    sandbox=""
+  ></iframe>
 
-      <iframe
-          src={randomnumimg}
-          alt=""
-          height={550}
-          width={1000}
-          style={{
-            marginLeft: "-30px",
-            marginBottom: "-350px",
-            paddingLeft: "-300px",
-          }}
-          className="randomno-video"
-          sandbox=""
-        ></iframe>
-      {/* <video
-        // src={randomnumimg}
-        // alt=""
-        height={550}
-        width={1000}
-        style={{
-          marginLeft: "-30px",
-          marginBottom: "-350px",
-          paddingLeft: "-300px",
-        }}
-        className="randomno-video"
-        // sandbox=""
-        >
-          <source src={randomnumimg} type="video/mp4" />
-        </video> */}
-      {/* <ReactPlayer url={randomvideo} /> */}
+  <div className="containerx">
+    {/* <div className="randomNum"> */}
+    <h1 className="txt-bold random_no_alignment">{randomNum}</h1>
+    {/* </div> */}
 
-      {/* <video
-        controls
-        // width="100%"
-        onClick={() => {
-          timeout();
-        }}
-        height={300}
-        width={1000}
-        style={{
-          marginLeft: "-30px",
-          marginBottom: "-350px",
-          paddingLeft: "-300px",
-        }}
-        className="randomno-video"
-      >
-        {/* <source src="/video-example.webm" type="video/webm" /> */}
-        {/* <source src={randomvideo} type="video/mp4" />
-      </video> */} */}
-
-      <div className="containerx">
-        {/* <div className="randomNum"> */}
-        <h1 className="txt-bold random_no_alignment">{randomNum}</h1>
-        {/* </div> */}
-
-        {/* <button
-            onClick={() => {
-              handleRandomNum();
-              timeOutFun();
-              setPlay(true);
-            }}
-            className="icon-conatiner random-no"
-          >
-            Spin
-          </button> */}
-      </div>
-      {/* </div> */}
-    </>
-  );
+    {/* <button
+      onClick={() => {
+        handleRandomNum();
+        timeOutFun();
+        setPlay(true);
+      }}
+      className="icon-conatiner random-no"
+    >
+      Spin
+    </button> */}
+  </div>
+</div>
+</>
+);
 }
