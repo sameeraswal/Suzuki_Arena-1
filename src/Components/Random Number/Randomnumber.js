339baa839bce49b92eb2f52efb76f30ac5bf8c89
Himanshu664 <gs2012023@sgsitsindore.in>
2023-03-15 00:26:56 +0530

    enterscore 1

--- src/Components/Random Number/Randomnumber.js
+++ src/Components/Random Number/Randomnumber.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./randomnum.css";
 import Navbar from "../Navbar";
 import { Link } from "react-router-dom";
-import randomnumimg from "./saucer final (1) (online-video-cutter.com).mp4";
+// import randomnumimg from "./saucer final (1) (online-video-cutter.com).mp4";
+import ReactPlayer from "react-player";
+import randomvideo from "./random.mp4";
 
 // const randNo=0;
 export default function Random() {
@@ -11,7 +13,7 @@ export default function Random() {
   // const [maxVal, setMaxVal] = useState(10);
   const [randomNum, setRandomNum] = useState("");
   const timeout = () => {
-    console.log("Rando,m");
+    // alert("Rando,m");
     setTimeout(() => {
       setRandomNum(Math.floor(Math.random() * (30 - 1 + 1) + 1));
     }, 14000);
@@ -33,11 +35,12 @@ export default function Random() {
     <>
       <Navbar />
 
-      <div className="hero">
-        {/* <div className="round-box ">Number Generator</div> */}
-        {/* <div className="round-box">Random Number Generator</div> */}
+      {/* <div className="hero"> */}
+      {/* <div className="round-box ">Number Generator</div> */}
+      {/* <div className="round-box">Random Number Generator</div> */}
 
-        <iframe
+
+      <iframe
           src={randomnumimg}
           alt=""
           height={550}
@@ -50,13 +53,48 @@ export default function Random() {
           className="randomno-video"
           sandbox=""
         ></iframe>
+      {/* <video
+        // src={randomnumimg}
+        // alt=""
+        height={550}
+        width={1000}
+        style={{
+          marginLeft: "-30px",
+          marginBottom: "-350px",
+          paddingLeft: "-300px",
+        }}
+        className="randomno-video"
+        // sandbox=""
+        >
+          <source src={randomnumimg} type="video/mp4" />
+        </video> */}
+      {/* <ReactPlayer url={randomvideo} /> */}
 
-        <div className="containerx">
-          {/* <div className="randomNum"> */}
-          <h1 className="txt-bold random_no_alignment">{randomNum}</h1>
-          {/* </div> */}
+      {/* <video
+        controls
+        // width="100%"
+        onClick={() => {
+          timeout();
+        }}
+        height={300}
+        width={1000}
+        style={{
+          marginLeft: "-30px",
+          marginBottom: "-350px",
+          paddingLeft: "-300px",
+        }}
+        className="randomno-video"
+      >
+        {/* <source src="/video-example.webm" type="video/webm" /> */}
+        {/* <source src={randomvideo} type="video/mp4" />
+      </video> */} */}
 
-          {/* <button
+      <div className="containerx">
+        {/* <div className="randomNum"> */}
+        <h1 className="txt-bold random_no_alignment">{randomNum}</h1>
+        {/* </div> */}
+
+        {/* <button
             onClick={() => {
               handleRandomNum();
               timeOutFun();
@@ -66,8 +104,8 @@ export default function Random() {
           >
             Spin
           </button> */}
-        </div>
       </div>
+      {/* </div> */}
     </>
   );
 }
